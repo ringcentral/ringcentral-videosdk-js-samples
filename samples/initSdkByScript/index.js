739bee236a5dc7c3f590381902895f815ef8917a
lina.qiu <lina.qiu@ringcentral.com>
2022-09-03 21:28:18 +0800

    add new sample

---
+++ samples/initSdkByScript/index.js
@@ -0,0 +1,29 @@
+const config = {
+    clientId: 'your clientID',
+    clientSecret: 'your client secret',
+    token: {
+        access_token: "xxx",
+        endpoint_id: "xxx",
+        expires_in: 3600,
+        owner_id: "xxx",
+        refresh_token: "xxx",
+        refresh_token_expires_in: 604800,
+        scope: "ReadAccounts Meetings SubscriptionWebhook",
+        token_type: "bearer"
+    }
+}
+
+function initSDK() {
+    const { clientId, clientSecret, token } = config
+    const RcvEngine = window.librcv.RcvEngine;
+    const engine = RcvEngine.create(
+        {
+            clientId,
+            clientSecret
+        }
+    );
+    engine.setAuthToken(JSON.stringify({ token }));
+    alert('Success!')
+}
+
+initSDK()
