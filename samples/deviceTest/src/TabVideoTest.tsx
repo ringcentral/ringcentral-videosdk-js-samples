d4689d9438e0be72f49cf50630e62e7f6da1c79d
lina.qiu <lina.qiu@ringcentral.com>
2022-10-14 20:59:53 +0800

    add setVideoDevice api

--- samples/deviceTest/src/TabVideoTest.tsx
+++ samples/deviceTest/src/TabVideoTest.tsx
@@ -1,6 +1,6 @@
-import React, { FC, useMemo, useRef, useState } from 'react'
+import React, { FC, useMemo, useRef, useState, useEffect } from 'react'
 import { RcvEngine } from '@sdk'
-import { RcButton } from '@ringcentral/juno';
+import { RcButton, RcSelect, RcMenuItem } from '@ringcentral/juno';
 interface IProps {
     rcvEngine: RcvEngine
 }
@@ -8,11 +8,28 @@ interface IProps {
 const TabVideoTest: FC<IProps> = ({ rcvEngine }) => {
     const videoRef = useRef<HTMLVideoElement>(null);
     const [isTesting, setIsTesting] = useState<boolean>(false);
+    const [deviceList, setDeviceList] = useState<MediaDeviceInfo[]>([]);
+    const [activeDevice, setActiveDevice] = useState('');
 
     const videoDeviceManager = useMemo(
         () => rcvEngine?.getVideoDeviceManager(),
         [rcvEngine, rcvEngine?.getVideoDeviceManager()]
     );
+
+    useEffect(() => {
+        videoDeviceManager.enumerateVideoDevices().then(deviceList => {
+            if (!deviceList.length) {
+                return;
+            }
+            setDeviceList(deviceList);
+            setActiveDevice(deviceList[0].deviceId);
+        });
+    }, []);
+
+    const handleVideoDeviceChange = e => {
+        setActiveDevice(e.target.value);
+        videoDeviceManager.setVideoDevice(e.target.value);
+    };
 
     const handleVideoTestStart = () => {
         setIsTesting(true);
@@ -29,8 +46,12 @@ const TabVideoTest: FC<IProps> = ({ rcvEngine }) => {
 
     return (
         <div className='card-wrapper'>
-            <h5>Video device test</h5>
-            <div>
+            <RcSelect label="Set Video Device" value={activeDevice} style={{ width: 200 }}
+                onChange={handleVideoDeviceChange}>
+                {deviceList.map((item) => <RcMenuItem key={item.deviceId} value={item.deviceId}> {item.label}</RcMenuItem>)}
+            </RcSelect>
+            <div style={{ marginTop: 20 }}>
+                <h5>Video device test</h5>
                 <RcButton onClick={handleVideoTestStart} color="success.b03" disabled={isTesting}>Start Video Test</RcButton>
                 <RcButton onClick={handleVideoTestStop} color="danger.b03">Stop Video Test</RcButton>
             </div>
